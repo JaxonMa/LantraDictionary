a7dcf0629b71b9d78cebf625bf918a4f654c6748
Caff <cafforite@outlook.com>
2026-01-17 01:07:29 +0800

    添加了dictionary.get_target_data()函数

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,7 @@
 
 # TODO: 查词功能。先查询polyphone.json, 如果是单字，再查询char_detail.json,并且查询related_char.json。如果是词语，则查询word.json
 # TODO: 放弃Vibe Coding提供的代码逻辑，先完成后端的数据处理。可以直接查到数据之后丢给前端
-import json
+import ijson
 
 DICTIONARY_PATH = {
     'chinese': {
@@ -13,6 +13,27 @@ DICTIONARY_PATH = {
         'word': 'dictionary/chinese/word.json'
     }
 }
+
+
+def get_target_data(query: str, lang: str, dict_name: str) -> dict[str, str]:
+    """
+    获得目标字典中指定的数据
+    :param query: 要查询的字词
+    :param lang: 字词的语言，可选值为 'chinese', 'english'
+    :param dict_name: 词典的名字。根据lang的不同，其对应的可选的词典名也会发生变化
+    :return: 指定字词的数据。
+        e.g: {'index': 6, 'char': '厂', 'strokes': 2, 'pinyin': ['chǎng', 'ān', 'hàn'], 'frequency': 0}
+    """
+    dict_path = DICTIONARY_PATH[lang]
+    target_data = {}
+    with open(dict_path[dict_name], 'r', encoding='utf-8') as f:
+        parser = ijson.items(f, 'item')
+        for item in parser:
+            if item['char'] == query:
+                target_data = item
+                break
+
+    return target_data
 
 
 def lookup(query: str, lang: str) -> dict[str, str]:
@@ -34,16 +55,12 @@ def lookup(query: str, lang: str) -> dict[str, str]:
             # 输入内容为单字，查询字典
             if len(query) == 1:
                 # 查询多音字字典，获得所有发音
-                with open(dict_path['polyphone'], 'r', encoding='utf-8') as f:
-                    polyphone_data = json.load(f)
-                    word_polyphone = [p for p in polyphone_data if p['char'] == query][0]  # 要的是字典而非列表，而这些列表必定只有一项，因此直接取[0]即可
-                    char_explanation['pinyin'] = word_polyphone['pinyin']
+                polyphone_data = get_target_data(query, 'chinese', 'polyphone')
+                char_explanation['pinyin'] = polyphone_data['pinyin']
 
                 # 查询释义字典，获得词语解释
-                with open(dict_path['char'], 'r', encoding='utf-8') as f:
-                    char_data = json.load(f)
-                    char_explanation_data = [e for e in char_data if e['char'] == query][0]
-                    char_explanation['pronunciations'] = [e for e in char_explanation_data['pronunciations']]
+                explanation_data = get_target_data(query, 'chinese', 'char')
+                print(explanation_data)
 
                 # 查询同义词词典，获得同义词列表
                 with open(dict_path['related_char'], 'r', encoding='utf-8') as f:
