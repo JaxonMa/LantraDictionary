e1a6a56dc4dd17d072c8f59182f571cc7dd95ce7
Jaxon Ma <cafforite@outlook.com>
2026-01-31 23:34:43 +0800

    Change dictionary data source to simplified ones.

--- dictionary.py
+++ dictionary.py
@@ -28,83 +28,23 @@ def lookup(query,) -> dict[str, str]:
     Returns:
         dict[str, str]: 查询结果
     """
-    char_result = {
-        "char": query,
-        "pinyin": [],
-        "radicals": "",
-        "pronunciations": [],
-        "related_char": []
-    }
-    word_result = {
-        "word": query,
-        "pinyin": "",
-        "explanation": [],
-    }
+    language = 'zh'
 
-    lang = 'zh'  # 目前仅支持中文查询
-
-    if len(query) == 1:
-        # 查询单个汉字  
-        with open(DICTIONARY_PATH[lang]['char_base'], 'r', encoding='utf-8') as f:
-            # 获得汉字基本信息
+    if query == 1:
+        # 查询单个汉字
+        with open(DICTIONARY_PATH[language]['char_base'], 'r', encoding='utf-8') as f:
             for item in ijson.items(f, 'item'):
                 if item['char'] == query:
-                    char_result['pinyin'] = item.get('pinyin', [])
-                    char_result['radicals'] = item.get('radicals', [])
-                    break
-
-        with open(DICTIONARY_PATH[lang]['char_detail'], 'r', encoding='utf-8') as f:
-            # 获得汉字详细解释
-            for item in ijson.items(f, 'item'):
-                if item['char'] == query:
-                    pronunciations = item.get('pronunciations', [])
-                    pinyin_explanation = {
-                        "pinyin": "",
-                        "explanation": []
-                    }
-
-                    for pron in pronunciations:
-                        # 筛选原始数据中的信息
-                        pinyin_explanation['pinyin'] = pron.get('pinyin', '')
-                        explanations = pron.get('explanations', [])
-                        for exp in explanations:
-                            if "content" in exp:
-                                pinyin_explanation['explanation'].append(exp['content'])
-                        # 同一拼音的解释已经结束
-                        char_result['pronunciations'].append(pinyin_explanation)
-                        pinyin_explanation = {
-                            "pinyin": "",
-                            "explanation": []
-                        }
-                    break
-
-        with open(DICTIONARY_PATH[lang]['related'], 'r', encoding='utf-8') as f:
-            # 获得相关汉字
-            for item in ijson.items(f, 'item'):
-                if item['char'] == query:
-                    char_result['related_char'] = item.get('synonyms', [])
-                    break
-        return char_result
-    
+                    return item
+        
     else:
-        # 查询词语
-        with open(DICTIONARY_PATH[lang]['word'], 'r', encoding='utf-8') as f:
+        # 查询汉字词语
+        with open(DICTIONARY_PATH[language]['word'], 'r', encoding='utf-8') as f:
             for item in ijson.items(f, 'item'):
                 if item['word'] == query:
-                    pinyin = item.get('pinyin', '').split()
-                    pinyin_processed = []
-
-                    for p in pinyin:
-                        # 处理拼音中的大写英文字母，在其之间添加空格，保证前端拼音显示正确
-                        if p[0].isupper():
-                            pinyin_processed.append(' '.join(list(p)))
-                        else:
-                            pinyin_processed.append(p)
-
-                    word_result['pinyin'] = ' '.join(pinyin_processed)
-                    word_result['explanation'] = item.get('explanation', [])
-                    break
-        return word_result
+                    return item
+                
+    raise ValueError(f"Query '{query}' not found in the dictionary.")
 
 
 if __name__ == '__main__':
