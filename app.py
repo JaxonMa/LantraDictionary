16179be15301bd1d331a0100771bb9a8cb5689ec
Jaxon Ma <cafforite@outlook.com>
2026-02-13 21:47:01 +0800

    Remove unnecessary CORS.

--- app.py
+++ app.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, jsonify
-from flask_cors import CORS
 
 import dictionary
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/')
